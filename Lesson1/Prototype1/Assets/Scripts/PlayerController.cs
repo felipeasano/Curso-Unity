0a18a4502c5bfc16830d27a11aa302c0e7509100
Jet Lee <asanofelipe@gmail.com>
2023-06-05 16:01:28 -0300

    Lesson 1.4 - Step into the Drivers Seat

--- Lesson1/Prototype1/Assets/Scripts/PlayerController.cs
+++ Lesson1/Prototype1/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //o caracter "f" entra no final do numero para indicar seu tipo, no caso, um "float"
-    public float speed = 5.0f;
+    // O caracter "f" entra no final do numero para indicar seu tipo, no caso, um "float"
+    // Private Variables 
+    private float speed = 10.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +19,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         // We'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        // Rotates the car based on horizontal input
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
     }
 }
